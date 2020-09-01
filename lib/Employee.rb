a4e0e37b69eca7116b4f08ad6f7267b033f166bc
denimcouch <alexmata92@ymail.com>
2020-09-01 14:53:35 -0500

    solution

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,30 @@
 class Employee
+    attr_reader :name, :salary, :manager
 
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        self.class.all << self
+    end
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(amount)
+        self.all.select{|e| e.salary > amount}
+    end
+    
+    def self.find_by_department(department)
+        self.all.find{|e| e.manager.department == department}
+    end
+
+    def tax_bracket
+        self.class.all.select{|e| 
+            (self.salary - e.salary).between?(-1000,1000) && e != self
+        }
+    end
+    
 end
