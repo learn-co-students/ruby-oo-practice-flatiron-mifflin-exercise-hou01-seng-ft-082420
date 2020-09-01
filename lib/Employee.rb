ad7cef557b369c7b1d1915e21a449ea787a1b12b
mlarcher531 <mlarcher531@gmail.com>
2020-09-01 14:52:43 -0500

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,32 @@
 class Employee
+    
+    attr_reader :name, :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(pay)
+        @@all.select {|e| e.salary > pay}
+    end
+
+    def self.find_by_department(department)
+        Employee.all.find {|e| e.manager.department == department}
+    end
+
+    def tax_bracket
+        # self.class.all.select {|e|
+        # ((self.salary - e.salary) <= 1000) && ((e.salary - self.salary) <= 1000) && e != self)}
+        self.class.all.select{|e| (self.salary - e.salary).between?(-1000,1000) && e != self}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,14 @@ require_relative "lib/Employee"
 require 'pry'
 
 
-#Test your code here
+m1 = Manager.new("Mike", "baking", 29)
+m2 = Manager.new("J", "AML", 25)
+
+e1 = Employee.new("Alex", 60000, m1)
+e2 = Employee.new("Marcus", 59005, m1)
+e3 = Employee.new("Stephen", 60500, m2)
+e4 = Employee.new("Mike", 80000, m2)
+
 
 
 binding.pry
